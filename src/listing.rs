3813659e9db0887ecd7557ff6c90599aa2fca6ed
jaidennnnn <48765522+jaidennnnn@users.noreply.github.com>
2025-12-04 13:30:54 +0000

    Make guild_ids mandatory

--- src/listing.rs
+++ src/listing.rs
@@ -189,7 +189,7 @@ pub struct DiscordServer {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GetServersRequest {
     pub user_id: String,
-    pub guild_ids: Option<Vec<String>>,
+    pub guild_ids: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
